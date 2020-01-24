68307f4e665d7c23c0b0756a13e7b414db84f004
Chris7721 <anagboguchristopher@gmail.com>
2020-01-24 19:07:34 +0100

    adding even numbers from fiboonacci series

---
+++ problem-2-even-fibonacci-numbers.js
@@ -0,0 +1,21 @@
+function fiboEvenSum(n) {
+  // You can do it!
+  let sum = 0;
+  let arr = fibonacci(n);
+  for(let i = 0; i<arr.length; i++){
+    if(arr[i] % 2 == 0){
+      sum+= arr[i];
+    }
+  }
+  return sum;
+}
+
+function fibonacci(num) {
+  let sol = [1,2];
+  for (let i = 2; i < num; i++) {
+    sol.push(sol[i-1] + sol[i-2]);
+  }
+  return sol;
+}
+
+// fiboEvenSum(10);
