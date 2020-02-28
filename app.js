0924916ea459d639a4fafa2450da4ab24ea26554
Chris7721 <anagboguchristopher@gmail.com>
2020-02-28 09:40:25 +0100

    find nth prime

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-function sumOfSquares(n){
-    let sum = 0;
-    for(let i = 1; i <= n; i++){
-      sum+= Math.pow(i, 2);  
-    }
+function nthPrime(n) {
+  // Good luck!
+  return firstNPrimeNumbers(n);
 }
 
-function squareOfSum(n){
-    let sum = 0;
-    for(let i = 1; i <= n; i++){
-        sum += i;  
+let primes = [2];
+function isPrime(num){
+    for(let i = 2; i <= Math.ceil(Math.sqrt(num)); i++){
+        if(num % i == 0){
+            return false;
+        }
     }
-    Math.pow(sum, 2);
+  return true;
 }
+function firstNPrimeNumbers(n){    
+    let i = 3;
+    do {
+      
+      // console.log(i);
+        if(isPrime(i)){
+        primes.push(i);
+      }i = i + 2;
+      
+    } while (primes.length < n);
+    console.log(primes);
+    if(primes.length > 0){
+    return primes[primes.length-1]
+  }
+    return [0] 
+}
+console.log(nthPrime(5));
+
+https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-7-10001st-prime
